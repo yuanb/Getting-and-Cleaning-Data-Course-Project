dd3b832b7e652efd90ed4f229cc5cadb09578bd0
yuanb <yuanb@users.noreply.github.com>
2019-04-23 15:35:45 -0400

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+#File name : run_analysis.R
+#Purpose : Assignment for 'Course 3 : Getting and Cleaning Data' Week 4
+
+
+#download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip', destfile = 'getdata_projectfiles_UCI HAR Dataset.zip')
+#unzip('getdata_projectfiles_UCI HAR Dataset.zip')
+
+# Move to 'UCI HAR Dataset' folder
+setwd('./UCI HAR Dataset')
